237b5ed22c3d76d5463b2432050327c132b6a8cd
sydung22 <72313449+sydung22@users.noreply.github.com>
2022-06-03 23:02:04 -0600

    addnew

--- source/database/factories/UserFactory.php
+++ source/database/factories/UserFactory.php
@@ -23,6 +23,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail(),
             'age' => $this->faker->numberBetween(18, 60),
             'gender' => $this->faker->randomElement(['male', 'female', 'other']),
+            'description' => $this->faker->realText(50),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
--- source/database/seeders/VideoSeeder.php
+++ source/database/seeders/VideoSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Hashtag;
 use App\Models\Video;
+use Illuminate\Support\Str;
 
 class VideoSeeder extends Seeder
 {
@@ -23,6 +24,7 @@ class VideoSeeder extends Seeder
                 'user_id' => mt_rand(1, 10),
                 'url' => 'https://res.cloudinary.com/dswt194ko/video/upload/v1651981448/videoUser/video_2_onsyif.mp4',
                 'cover' => 'https://via.placeholder.com/150x150/'.substr(md5(rand()), 0, 6),
+                'description' => Str::random(20),
             ]);
             $video->hashtags()->syncWithoutDetaching($hashtags);
         }
