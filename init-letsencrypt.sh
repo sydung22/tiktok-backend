c525f83a0b6e014c2784b11723d9ec0adbd63af6
AnhHoangQuach <hoanganh36.work@gmail.com>
2022-06-25 02:20:53 +0700

    minor

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(utenew.tk)
 rsa_key_size=4096
 data_path="./compose/nginx/certbot"
-email="" # Adding a valid address is strongly recommended
+email="hoanganh36.work@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
